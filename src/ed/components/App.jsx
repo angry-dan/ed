2e5922619c6d56ca5d003b563e3fe5ccc41cea0b
Dan James <ignitedfirestarter@gmail.com>
2017-02-28 20:55:56 +0000

    Drag/drop. Not re-ordering (yet)

--- src/ed/components/App.jsx
+++ src/ed/components/App.jsx
@@ -1,11 +1,14 @@
 import Editor from "./Editor";
 import Preview from "./Preview";
 import AddButton from "./AddButton";
-
 import React from "react";
-import '../ed.css';
+import "../ed.css";
 
-export default (props) => <div className="row">
+import HTML5Backend from 'react-dnd-html5-backend';
+
+import { DragDropContext } from 'react-dnd';
+
+const component = (props) => <div className="row">
   <div className="col m6 z-depth-2 blue darken-1 editor--left-col">
     <Editor />
     <AddButton />
@@ -15,3 +18,4 @@ export default (props) => <div className="row">
   </div>
 </div>;
 
+export default DragDropContext(HTML5Backend)(component);
--- src/ed/components/ComponentForm.jsx
+++ src/ed/components/ComponentForm.jsx
@@ -1,8 +1,10 @@
 import React, {Component, createElement} from "react";
-export default class extends Component {
+import { DragSource } from 'react-dnd';
+
+class component extends Component {
   render() {
     const formProps = Object.assign({onChange: this.props.onChange}, this.props.componentState);
-    return <div className="card white">
+    return this.props.connectDragPreview(<div className="card white">
       <div className="card-content">
         {createElement(this.props.component.componentForm, formProps)}
       </div>
@@ -10,10 +12,32 @@ export default class extends Component {
         <button onClick={this.props.onDelete} className="waves-effect waves-light btn red">
           Delete
         </button>
-        <button className="btn btn-floating waves-effect waves-light red">
-          <i className="material-icons">reorder</i>
-        </button>
+        {this.props.connectDragSource(
+          <button className="btn btn-floating red">
+            <i className="material-icons">reorder</i>
+          </button>
+        )}
       </div>
-    </div>
+    </div>)
+  }
+}
+
+const source = {
+  beginDrag(props) {
+    return {i : props.i};
   }
 };
+
+
+// Basically, this: https://github.com/react-dnd/react-dnd/blob/master/examples/04%20Sortable/Simple/Card.js
+// You could form that into a container component that deals with the dragging and dropping.
+// That would be a good idea allow this component to be better re-used.
+function collect(connect, monitor) {
+  return {
+    connectDragSource: connect.dragSource(),
+    connectDragPreview: connect.dragPreview(),
+    isDragging: monitor.isDragging()
+  };
+}
+
+export default DragSource('ComponentForm', source, collect)(component)
