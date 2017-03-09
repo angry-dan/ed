dc40528d45ad168a806f8861a726f61e21d763c1
Dan James <ignitedfirestarter@gmail.com>
2017-03-09 07:37:33 +0000

    Magic add button

--- src/ed/components/AddButton.jsx
+++ src/ed/components/AddButton.jsx
@@ -2,25 +2,18 @@ import React from "react";
 import {connect} from "react-redux";
 import {addComponent} from "../modules/components";
 
-const component = ({addComponent, to = 0}) => <div>
+export const component = ({addComponent, componentTypes,  to = 0}) => <div>
   <a className="btn-floating red btn-large">
     <i className="large material-icons">add</i>
   </a>
   <ul>
-    <li>
-      <button title="Add header" className="btn-floating red" onClick={() => addComponent('header', to)}>
-        <i className="material-icons">format_quote</i></button>
-    </li>
-    <li>
-      <button title="Add body" className="btn-floating yellow darken-1" onClick={() => addComponent('body', to)}>
-        <i className="material-icons">format_quote</i></button>
-    </li>
-    <li>
-      <button title="Add container" className="btn-floating pink darken-1" onClick={() => addComponent('container', to)}>
-        <i className="material-icons">format_quote</i></button>
-    </li>
+    {Object.keys(componentTypes).map(type => <li key={type}>
+        <button title={"Add " + componentTypes[type].description} className="btn-floating red" onClick={() => addComponent(type, to)}>
+          <i className="material-icons">format_quote</i></button>
+      </li>
+    )}
   </ul>
 </div>;
 
-export default connect(undefined, {addComponent})(component);
+export default connect(state => ({componentTypes: state.componentTypes}), {addComponent})(component);
 // TODO read component types and expose buttons for each one.
