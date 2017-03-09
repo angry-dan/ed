4105a11dbac2423357b5a09c97a074b51580f8c0
Dan James <ignitedfirestarter@gmail.com>
2017-03-09 07:37:04 +0000

    Register container component. And support for redux dev tools.

--- src/ed.jsx
+++ src/ed.jsx
@@ -7,6 +7,7 @@ import componentTypes from "./ed/modules/componentTypes";
 import components from "./ed/modules/components";
 import header from "./components/header";
 import body from "./components/markdown";
+import container from "./components/container";
 
 function reducer(state = {}, action = {}) {
   return {
@@ -15,7 +16,7 @@ function reducer(state = {}, action = {}) {
   }
 }
 
-const store = createStore(reducer, {componentTypes: {header, body}});
+const store = createStore(reducer, {componentTypes: {header, body, container}}, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 render(
   <Provider store={store}>
